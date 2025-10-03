de0c6a2cbb7e15d72bef5b85f33461e78351782f
nicolas <Kiko2008.emilio@gmail.com>
2025-10-03 19:24:53 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+string fraseDigitada, fraseComDislalia;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Digite uma frase: ");
+fraseDigitada = Console.ReadLine()!;
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+fraseComDislalia = fraseDigitada
+.Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(fraseComDislalia);
+Console.ResetColor();
